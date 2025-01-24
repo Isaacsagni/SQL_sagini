4a2da174c5505f13e518b46224021251bbbf85bb
Isaacsagni <160358867+Isaacsagni@users.noreply.github.com>
2025-01-24 04:25:39 +0300

    Duplicatesrow.sql
    This is for the initial data

--- Duplicatesrow.sql
+++ Duplicatesrow.sql
@@ -1,4 +1,4 @@
 
 SELECT *,
     ROW_NUMBER() OVER (PARTITION BY company, location, industry, percentage_laid_off, `date`, stage, country, funds_raised_millions) AS row_num
-FROM layoffs_staging;
+FROM layoffs;
